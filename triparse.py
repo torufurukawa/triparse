34a0ba5979333c63d06e780ea2c58aeece477fef
Toru Furukawa <torufurukawa@gmail.com>
2016-10-08 16:01:32 +0900

    Add parsing line

--- triparse.py
+++ triparse.py
@@ -1,6 +1,7 @@
 """Parse PDF that contains triathlon race result"""
 
 import sys
+import re
 import codecs
 
 
@@ -8,11 +9,17 @@ def main():
     # TODO: use argparse
     fin = open(sys.argv[1], encoding='utf8')
     for line in fin:
-        print(line.strip())
 
-    # TODO: parse
-    # TODO: print
-    pass
+        # TODO:DOING parse
+        pattern = r'\d+ (\d+).*?\d:\d{2}:\d{2} (\d:\d{2}:\d{2}) \d+ (\d:\d{2}:\d{2}) \d+ \d:\d{2}:\d{2} \d+ (\d:\d{2}:\d{2}) \d+(\S+)'
+        m = re.match(pattern, line)
+        if m is None:
+            continue
+        aid, swim_str, bike_str, run_str, div = m.groups()
+
+        # TODO: print
+        pass
+
     # TODO: analyze and visualize
 
 
