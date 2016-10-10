e42b09d41d56d7b9b2f3404365a0d3afd997e390
Toru Furukawa <torufurukawa@gmail.com>
2016-10-10 18:10:59 +0900

    Add parsing lap times

--- triparse.py
+++ triparse.py
@@ -3,6 +3,7 @@
 import sys
 import re
 import codecs
+from datetime import timedelta
 
 
 def main():
@@ -10,17 +11,28 @@ def main():
     fin = open(sys.argv[1], encoding='utf8')
     for line in fin:
 
-        # TODO:DOING parse
+        # TODO: fix style
         pattern = r'\d+ (\d+).*?\d:\d{2}:\d{2} (\d:\d{2}:\d{2}) \d+ (\d:\d{2}:\d{2}) \d+ \d:\d{2}:\d{2} \d+ (\d:\d{2}:\d{2}) \d+(\S+)'
         m = re.match(pattern, line)
         if m is None:
             continue
         aid, swim_str, bike_str, run_str, div = m.groups()
 
-        # TODO: print
-        pass
+        swim_lap = parse_lap(swim_str)
+        bike_lap = parse_lap(bike_str)
+        run_lap = parse_lap(run_str)
+
+        print(aid, swim_lap, bike_lap, run_lap, div)
 
     # TODO: analyze and visualize
+
+
+def parse_lap(lap_str):
+    """Parse string H:MM:SS and return timedelta"""
+    pattern = r'(\d):(\d{2}):(\d{2})'
+    hours, minutes, seconds = map(int, re.match(pattern, lap_str).groups())
+    result = timedelta(hours=hours, minutes=minutes, seconds=seconds)
+    return result
 
 
 if __name__ == '__main__':
