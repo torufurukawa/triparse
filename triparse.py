ab4bf041d484c356607c2c368b408cb6df485e50
Toru Furukawa <torufurukawa@gmail.com>
2016-10-21 20:19:45 +0900

    Refactor

--- triparse.py
+++ triparse.py
@@ -114,30 +114,31 @@ def result_reader(textfile):
     textfile is sequence of single line texts that contain athete results
     """
     for line in textfile:
-
-        # TODO: make parser plaggable
-        # TODO: fix style
-        pattern = r'\d+ (\d+).*?\d:\d{2}:\d{2} (\d:\d{2}:\d{2}) \d+ (\d:\d{2}:\d{2}) \d+ \d:\d{2}:\d{2} \d+ (\d:\d{2}:\d{2}) \d+(\S+)'
-        m = re.match(pattern, line)
-        if m is None:
+        result = parse_result_line(line)
+        if result is None:
             continue
-        aid, swim_str, bike_str, run_str, div = m.groups()
-
-        swim_lap = parse_lap(swim_str)
-        bike_lap = parse_lap(bike_str)
-        run_lap = parse_lap(run_str)
-
-        result = Result(aid, swim_lap, bike_lap, run_lap, div)
-
         yield result
 
 
-def parse_lap(lap_str):
-    """Parse string H:MM:SS and return timedelta"""
-    pattern = r'(\d):(\d{2}):(\d{2})'
-    hours, minutes, seconds = map(int, re.match(pattern, lap_str).groups())
-    result = timedelta(hours=hours, minutes=minutes, seconds=seconds)
-    return result
+def parse_result_line(line):
+    pattern = r'\d+ (\d+).*?\d:\d{2}:\d{2} (\d:\d{2}:\d{2}) \d+ (\d:\d{2}:\d{2}) \d+ \d:\d{2}:\d{2} \d+ (\d:\d{2}:\d{2}) \d+(\S+)'
+    m = re.match(pattern, line)
+    if m is None:
+        return None
+    aid, swim_str, bike_str, run_str, div = m.groups()
+
+    def parse_lap(lap_str):
+        """Parse string H:MM:SS and return timedelta"""
+        pattern = r'(\d):(\d{2}):(\d{2})'
+        hours, minutes, seconds = map(int, re.match(pattern, lap_str).groups())
+        result = timedelta(hours=hours, minutes=minutes, seconds=seconds)
+        return result
+
+    swim_lap = parse_lap(swim_str)
+    bike_lap = parse_lap(bike_str)
+    run_lap = parse_lap(run_str)
+
+    return Result(aid, swim_lap, bike_lap, run_lap, div)
 
 
 if __name__ == '__main__':
